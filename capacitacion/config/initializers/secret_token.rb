dc3e9ac325da259bbf5af1ae9a0dcd3e37963415
NicoRisso <narisso@uc.cl>
2013-09-03 14:42:01 -0400

    Rails en blanco

---
+++ capacitacion/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capacitacion::Application.config.secret_token = '59b6eb79f8559b0331aed8aff8b3584cfb389218292abc395bfbe250689a4fd551b0e5bb150d655c97d58e42fee53f4ccb2a727991e666412c9b05ec9fffcb7e'
