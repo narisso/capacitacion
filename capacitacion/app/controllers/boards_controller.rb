d3c6cc7ac22077532036d4ac697ad83f6f167921
Thomas Büchi Sagredo <thombuchi@gmail.com>
2013-09-05 14:41:10 -0400

    create

--- capacitacion/app/controllers/boards_controller.rb
+++ capacitacion/app/controllers/boards_controller.rb
@@ -9,4 +9,18 @@ class BoardsController < ApplicationController
 	end
 
 
+
+	def new
+	end
+
+	def create
+		@board = Board.new(paramas[:title])
+		@board.save
+	end
+
+	private
+		def board_params
+			params.require(:board).permit(:title, :text)
+		end
+
 end
